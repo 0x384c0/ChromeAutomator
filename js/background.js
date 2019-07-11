6d2063b0eea2f03839981b02a6f78fd31d901901
0x384c0 <0x384c0@gmail.com>
2019-07-11 16:20:25 +0600

    added ui commands

--- js/background.js
+++ js/background.js
@@ -22,40 +22,66 @@ function sendMessage(tabId, message) {
     });
 }
 
+//debugger
 
+var attached = false
 var version = "1.2";
 function connectDebugger(tab) {
     return new Promise(function (resolve, reject) {
         var tabId = tab.id;
         var debuggeeId = { tabId: tabId };
-        chrome.debugger.attach(debuggeeId, version, () => {
-            if (chrome.runtime.lastError)
-                reject(chrome.runtime.lastError)
-            else
-                resolve(debuggeeId)
-        });
+        if (attached) {
+            resolve(debuggeeId)
+        } else {
+            chrome.debugger.attach(debuggeeId, version, () => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError)
+                } else {
+                    attached = true
+                    resolve(debuggeeId)
+                }
+            });
+        }
     })
 }
+function onDetach(debuggeeId) {
+    attached = false
+}
 
-// other utility Promisified methods
-function wait(ms) {
+
+// internal utils
+function print(obj) {
+    console.log(obj)
+}
+
+function setIntervalX(delay, repetitions, callback, finished) {
+    var x = 0;
+    var intervalID = window.setInterval(function () {
+        callback();
+        if (++x === repetitions) {
+            window.clearInterval(intervalID);
+            finished()
+        }
+    }, delay);
+    return () => { window.clearInterval(intervalID) }
+}
+
+function sleep(ms) {
     return new Promise(function (resolve, reject) { window.setTimeout(resolve, ms); });
 }
+
 function clickSelector(target, selector) {
     return sendCommand(target, 'DOM.getDocument')
         .then(function (rootNode) {
             var rootNodeId = rootNode.root.nodeId;
-            console.log('rootNode', rootNode, rootNodeId);
             return sendCommand(target, 'DOM.querySelector', {
                 nodeId: rootNodeId,
                 selector: selector
             })
                 .then(function (node) {
-                    console.log('queried node:', node);
                     return sendCommand(target, 'DOM.getBoxModel', node);
                 })
                 .then(function ({ model: model }) {
-                    console.log(model);
                     var border = model.border;
                     var topLeftX = border[0];
                     var topLeftY = border[1];
@@ -77,25 +103,87 @@ function clickSelector(target, selector) {
         });
 }
 
-function start(tab) {
-    connectDebugger(tab)
-        .then((debuggeeId) => {
-            clickSelector(debuggeeId,"div[class='vjs-play-control vjs-control ']")
-        })
-        .catch((e) => {
-            alert("Error: " + e)
+function catchError(e) {
+    print(e)
+    alert(e.message)
+}
+
+//utils
+function click(debuggeeId, selector) {
+    return clickSelector(debuggeeId, selector)
+}
+
+function exists(debuggeeId, selector, regex) {
+    let code = "document.querySelector(\"" + selector + "\").innerText"
+    return executeScript(debuggeeId.tabId, { code: code })
+        .then((innerText) => {
+            let array = innerText.toString().match(regex)
+            let result = array != undefined && array != null && array.length != 0
+            if (!result) {
+                print("found element for selector: " + selector + " regex: " + regex + " but innerText was: " + innerText)
+            }
+            return result
         })
 }
 
+function wait(debuggeeId, selector, regex, timout) {
+    const delay = 1000
+    const repetitions = timout / delay
+    return new Promise((resolve, reject) => {
+        var clearInterval = setIntervalX(delay, repetitions,
+            () => {
+                exists(debuggeeId, selector, regex)
+                    .then((exists) => {
+                        if (exists) {
+                            clearInterval()
+                            resolve(exists)
+                        }
+                    })
+                    .catch(print)
+            },
+            () => {
+                reject(new Error("Element with selector: \"" + selector + "\" regex: \"" + regex + "\" not exists."))
+            }
+        )
+    })
+}
 
+function waitAndClick(debuggeeId, selector, regex, timout) {
+    return wait(debuggeeId, selector, regex, timout)
+        .catch(catchError)
+        .then(() => { return sleep(500) })
+        .then(() => { return click(debuggeeId, selector) })
+}
+
+function waitRequest(regex) {
+
+}
+
+//main
 function main() {
-    chrome.runtime.onMessage.addListener(onMessage)
+    chrome.debugger.onDetach.addListener(onDetach);
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.command == "start") {
+            sendResponse({ message: "started: " + request.targetTab.id });
+            return start(request.targetTab)
+        }
+    })
 }
-function onMessage(request, sender, sendResponse) {
-    if (request.command == "start") {
-        start(request.targetTab)
-        sendResponse({ message: "started: " + request.targetTab.id });
-    }
+
+function start(tab) {
+    return connectDebugger(tab)
+        .then(debuggeeId => { exeucteTask(debuggeeId) })
+        .catch(catchError)
+}
+
+function exeucteTask(debuggeeId) {
+    return click(debuggeeId, "div[class='vjs-play-control vjs-control ']")
+        .then(() => {
+            return wait(debuggeeId, "div[class='skip-button']", "^Пропустить рекламу$", 25000)
+        })
+        .then(() => {
+            print("Finished")
+        })
 }
 
 main()
