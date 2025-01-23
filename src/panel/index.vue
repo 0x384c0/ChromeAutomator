f938efd8a8839e9d259367e5f1695df1c2d4868e
0x384c0 <0x384c0@gmail.com>
2025-01-23 23:48:45 +0600

    added splitpanes

--- src/panel/index.vue
+++ src/panel/index.vue
@@ -1,36 +1,46 @@
 <template>
     <div class="panel-container">
-        <div>
-            <Dropdown v-model="selectedScriptTemplate" :options="scriptTemplates" />
-            <button v-if="!isWorking && !isStopping" @click="start">
-                Play
-            </button>
-            <button v-if="isWorking && !isStopping" @click="stop">
-                Stop
-            </button>
-            <button v-if="isWorking && !isStopping" @click="restart">
-                Restart
-            </button>
-            <input type="checkbox" v-model="isHighlighCurrentLine" @change="isHighlighCurrentLineChange">Highligh
-            current line</input>
-            <div v-if="isStopping">Stopping...</div>
-        </div>
-        <div id="editor-container" class="monaco-editor"></div>
-        <Output 
-            :content="outputText"
-            @clear="clearOutputText"
-            class="output-container"
-        />
+        <splitpanes class="default-theme" horizontal>
+            <pane min-size="10">
+                <div class="editor-wrapper">
+                    <div class="controls">
+                        <Dropdown v-model="selectedScriptTemplate" :options="scriptTemplates" />
+                        <button v-if="!isWorking && !isStopping" @click="start">
+                            Play
+                        </button>
+                        <button v-if="isWorking && !isStopping" @click="stop">
+                            Stop
+                        </button>
+                        <button v-if="isWorking && !isStopping" @click="restart">
+                            Restart
+                        </button>
+                        <input type="checkbox" v-model="isHighlighCurrentLine"
+                            @change="isHighlighCurrentLineChange">Highligh
+                        current line</input>
+                        <div v-if="isStopping">Stopping...</div>
+                    </div>
+                    <div id="editor-container" class="monaco-editor"></div>
+                </div>
+            </pane>
+            <pane min-size="10">
+                <Output :content="outputText" @clear="clearOutputText" class="output-container" />
+            </pane>
+        </splitpanes>
     </div>
 </template>
 
 <script>
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
 import { setupLogic } from './logic.ts';
 import Dropdown from './components/Dropdown.vue';
 import Output from './components/Output.vue';
 
 export default {
     components: {
+        Splitpanes,
+        Pane,
         Dropdown,
         Output,
     },
@@ -51,8 +61,14 @@ export default {
     bottom: 0;
 }
 
-.buttons-container {
+.controls {
     flex: 0 0 auto;
+}
+
+.editor-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
 }
 
 .monaco-editor {
