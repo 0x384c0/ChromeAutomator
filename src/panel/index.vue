6d505acea44547ffddc6c8b5caee00a3e5feae95
0x384c0 <0x384c0@gmail.com>
2025-01-23 23:54:20 +0600

    refactor

--- src/panel/index.vue
+++ src/panel/index.vue
@@ -5,18 +5,11 @@
                 <div class="editor-wrapper">
                     <div class="controls">
                         <Dropdown v-model="selectedScriptTemplate" :options="scriptTemplates" />
-                        <button v-if="!isWorking && !isStopping" @click="start">
-                            Play
-                        </button>
-                        <button v-if="isWorking && !isStopping" @click="stop">
-                            Stop
-                        </button>
-                        <button v-if="isWorking && !isStopping" @click="restart">
-                            Restart
-                        </button>
-                        <input type="checkbox" v-model="isHighlighCurrentLine"
-                            @change="isHighlighCurrentLineChange">Highligh
-                        current line</input>
+                        <button v-if="!isWorking && !isStopping" @click="start">Play</button>
+                        <button v-if="isWorking && !isStopping" @click="stop">Stop</button>
+                        <button v-if="isWorking && !isStopping" @click="restart">Restart</button>
+                        <input type="checkbox" v-model="isHighlighCurrentLine" @change="isHighlighCurrentLineChange">
+                        Highligh current line</input>
                         <div v-if="isStopping">Stopping...</div>
                     </div>
                     <div id="editor-container" class="monaco-editor"></div>
