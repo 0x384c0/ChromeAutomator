6ae1733e6abaca72d6817a796ad2441587a1c69f
0x384c0 <0x384c0@gmail.com>
2025-01-24 20:34:53 +0600

    refactor popup

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,6 +1,13 @@
 import { createApp, defineComponent, h } from 'vue'
 import App from './index.vue'
-import './index.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura';
+import '/node_modules/primeflex/primeflex.css'
 
 const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    }
+});
 app.mount('#app');
--- src/popup/index.vue
+++ src/popup/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <div class="text_view">
-      <div>
-        You will need the devtools "Crome Scrapper" panel opened.<br />
-        Activate the devtools panel with F12 or Ctrl+Shift+J.<br />
-        With the devtools open, find and select the "Crome Scrapper" tab from the top menu of the devtools.<br />
-        You may need to press the ">>" in order to find it.
-      </div>
+    <div class="text_view mb-3">
+      Activate the devtools panel with F12 or Ctrl+Shift+J.<br />
+      Find and select the "Crome Scrapper" tab from the top menu of the devtools.<br />
     </div>
-    <div id="settings">
-      <input type="checkbox" id="checkbox" v-model="isVerboseLogging" @change="isVerboseLoggingChange" />
-      <label for="checkbox">Verbose Logging</label>
+    <div class="flex align-items-center">
+      <InputSwitch type="checkbox" id="checkbox" v-model="isVerboseLogging" @change="isVerboseLoggingChange" />
+      <label for="checkbox" class="ml-2">Verbose Logging</label>
     </div>
   </div>
 </template>
 
 <script src="./popup_component.ts"></script>
+
+<style>
+body {
+  padding: 10px;
+  width: 300px;
+}
+</style>
